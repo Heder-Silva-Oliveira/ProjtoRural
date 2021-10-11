f274dd257e02f7ed934407983b9b99c5c1e4f540
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-10-11 18:19:29 -0300

    mudanças

--- Calculos.py
+++ Calculos.py
@@ -1,33 +1,41 @@
 pronta_meses = 14
 entregetacao_meses = 2
 gestacao_meses = 10
-ciclo = 12
-vacas = 10
-bezerras_totais = tempov = bezerras = tempob = tempo = 0
+ciclo = int(12)
+vacasini = 10
+bezerras = {}
+vacas = {}
+tempo = 0
 prazo = int(input("Quantos anos? ")) * 12
-
+tempov = (type(ciclo/prazo))
+tempob = (ciclo/prazo)
+for x in range(10):
+    bezerras[x] = 1
 
 while tempo <= prazo:
     tempo += 1
-    tempob += 1
-    tempov += 1
     #Inicio do projeto com vacas emprenhadas
-    if tempov == 10:
-        bezerras_totais = vacas
-        tempov = -2
-    elif bezerras_totais > 0:
+    if tempov == 12:
+        x = bezerras[len(bezerras)]
+        bezerras[x] = bezerras[len(bezerras)]
+        for k in vacas.items():
+            bezerras[x] = tempo
+
+
+    #proxima leva de gado
+    '''elif bezerras_totais > 0:
         if tempob == 14:
             vacas += bezerras_totais
             bezerras = bezerras_totais
             tempob = 0
             bezerras_totais = 0
         else:
-            pass
-    else:
-        pass
+            pass'''
 
 
-print(prazo)
-print(tempo)
-print(f'Quantiade de vacas {vacas}')
-print(f'Quantiade e bezerras {bezerras}')
+
+'''print(prazo)'''
+print(vacas)
+print(bezerras)
+'''print(f'Quantiade de vacas {vacas}')
+print(f'Quantiade e bezerras {bezerras}')'''
