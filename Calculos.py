9fd37bfd6c45000c441f995cccfbf5a4e138a852
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-11-07 08:53:53 -0300

    Segundo passo: 005

--- Calculos.py
+++ Calculos.py
@@ -2,37 +2,36 @@ pronta_meses = 14
 entregetacao_meses = 2
 gestacao_meses = 10
 ciclo = int(12)
-vacasini = cont = cont2 = 1
+vacasini = 10
+cont = cont2 = 1
 bezerras = {}
-vacas = {0:0, 1:1}
+vacas = {}
 tempo = outra = 0
 prazo = int(input("Quantos anos? ")) * 12
 numero = len(bezerras)
 
-
+for x in range(vacasini):
+    vacas[x] = 1
 tempv = 0
 while tempo < prazo:
     tempo += 1
     #Inicio do projeto com vacas emprenhadas
     for x in range(vacasini):
-        vacas[x] = tempv
-        tempv += 1
-        if vacas[x] == 12:
+        vacas[x] += 1
+        if vacas[x] >= 12:
             vacas[x] = 1
-
+    print(vacas)
 
     for k, i in vacas.items():
         resultado = int(vacas[k] / 12)
         tempv += 1
     #Adicionando bezerras
-        if resultado == cont:
-            cont += 1
+        if resultado in range(1, 50):
             outra = {cont: cont2}
             bezerras.update(outra)
 
-        else:
-            pass
-
+    else:
+        pass
 #proxima leva de gado
     for x, z in bezerras.items():
         bezerras[x] += 1
@@ -51,7 +50,7 @@ while tempo < prazo:
 
 
 
-print(outra)
+#print(outra)
 print(vacas)
 print(bezerras)
 '''print(f'Quantiade de vacas {vacas}')
